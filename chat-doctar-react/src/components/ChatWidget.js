8521b4b4f3ba861598c559a6c0c8f23cd6777066
sreetej1998 <41690429+sreetej1998@users.noreply.github.com>
2020-03-14 15:37:51 +0530

    Update ChatWidget.js

--- chat-doctar-react/src/components/ChatWidget.js
+++ chat-doctar-react/src/components/ChatWidget.js
@@ -17,7 +17,7 @@ const ChatWidget=(props)=>{
     },[])
 
     const sendMail= ()=>{
-      sgMail.setApiKey('SG.PGzz49Z6TvOfVr5w1n3Low.TuUw6pAGLxdKVfFp13gt9IiXLMZyHypxDFgal9sbr2gSG.PGzz49Z6TvOfVr5w1n3Low.TuUw6pAGLxdKVfFp13gt9IiXLMZyHypxDFgal9sbr2g');
+      sgMail.setApiKey('');
       const msg = {
         to: 'test@example.com',
         from: 'test@example.com',
